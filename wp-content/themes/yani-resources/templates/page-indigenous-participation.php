c09c33cf7665b3d00aa109b1bd226cb074bd0312
Oussema Sferi <oussema.sferi@gmail.com>
2025-12-01 03:27:14 +0100

    feat: add Partnering section to Indigenous Participation page
    - Add Partnering section with two-column layout (content left, image right)
    - Add ACF fields: title, content (WYSIWYG), subtitle, partners list (WYSIWYG), image
    - Add CSS matching Services page layout structure
    - White background section with vertically centered content
    - Image matches content height and is centered (same CSS as Services page)

--- wp-content/themes/yani-resources/templates/page-indigenous-participation.php
+++ wp-content/themes/yani-resources/templates/page-indigenous-participation.php
@@ -50,12 +50,43 @@ get_template_part( 'template-parts/components/hero' );
     </div>
 </section>
 
-<!-- Indigenous Participation Page Content -->
+<!-- Partnering Section -->
 <section class="indigenous-participation-page">
     <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <p>Indigenous Participation page content will go here. This section can be customized with ACF fields.</p>
+        <div class="row g-5">
+            <!-- Left Column: Content -->
+            <div class="col-md-6">
+                <div class="indigenous-participation-page__content-wrapper">
+                    <?php
+                    $partnering_title = get_field( 'indigenous_partnering_title', $page_id );
+                    $partnering_content = get_field( 'indigenous_partnering_content', $page_id );
+                    $partnering_subtitle = get_field( 'indigenous_partnering_subtitle', $page_id );
+                    $partnering_partners = get_field( 'indigenous_partnering_partners', $page_id );
+                    ?>
+                    
+                    <h2 class="indigenous-participation-page__title"><?php echo wp_kses_post( $partnering_title ); ?></h2>
+                    <div class="indigenous-participation-page__content"><?php echo apply_filters( 'the_content', $partnering_content ); ?></div>
+                    
+                    <?php if ( $partnering_subtitle ) : ?>
+                        <h3 class="indigenous-participation-page__subtitle"><?php echo esc_html( $partnering_subtitle ); ?></h3>
+                    <?php endif; ?>
+                    
+                    <div class="indigenous-participation-page__content"><?php echo apply_filters( 'the_content', $partnering_partners ); ?></div>
+                </div>
+            </div>
+            
+            <!-- Right Column: Image -->
+            <div class="col-md-6">
+                <div class="indigenous-participation-page__image-wrapper">
+                    <?php
+                    $partnering_image = get_field( 'indigenous_partnering_image', $page_id );
+                    if ( $partnering_image ) {
+                        echo '<img src="' . esc_url( $partnering_image ) . '" alt="Partnering with Yani" class="indigenous-participation-page__image">';
+                    } else {
+                        echo '<img src="' . esc_url( get_template_directory_uri() . '/assets/images/worker-electrician.jpg' ) . '" alt="Partnering with Yani" class="indigenous-participation-page__image">';
+                    }
+                    ?>
+                </div>
             </div>
         </div>
     </div>
